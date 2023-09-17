ff6e34b5de01dd7b94a671370a7aba2dc914b5b4
Nathany <nathanyk.morais@gmail.com>
2023-09-17 15:21:23 -0300

    implementação do Ecossistema da Segurança

--- src/main/java/com/generation/lojagames/model/Produto.java
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -41,6 +41,10 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -96,6 +100,14 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 }
